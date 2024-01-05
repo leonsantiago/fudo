484a83cfde08cd6f3c979d7ecb4a6baad661713b
Santiago Leon <santiago.leon@binomio.uy>
2024-01-04 22:42:13 -0300

    Get the route_info of the request

--- app/router.rb
+++ app/router.rb
@@ -16,4 +16,25 @@ class Router
   def not_found(msg = 'Not Found')
     [404, { "Content-Type:" => "application/json" }, [msg]]
   end
+
+  def route_info
+    @route_info ||= begin
+      resource = path_fragments[0] || 'main'
+      id, action = get_id_and_action(path_fragments[1])
+      
+      { resource: resource, action: action, id: id }
+    end
+  end
+
+  def get_id_and_action(fragmented_path)
+    case fragmented_path
+    when 'new'
+      [nil, :new]
+    when nil
+      action = @request.get? ? :index, :create
+      [nil, action]
+    else
+      [fragment, :show]
+    end
+  end
 end
