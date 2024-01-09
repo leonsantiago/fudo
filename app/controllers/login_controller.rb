a0556745ebfffb3205a37789f52a439c98c821a2
Santiago Leon <santiago.leon@binomio.uy>
2024-01-09 12:16:38 -0300

    Clean router

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,15 +3,17 @@ require_relative '../services/authentication_service.rb'
 
 class LoginController < MainController
   # /login
-  def login
-    username = params['username']
-    password = params['password']
 
+  def initialize(app)
+    @app = app
+  end
+
+  def call(request)
+    username, password = get_user_and_password(request)
     token = generate_token(username, password)
     body = { token: token }
 
     token ? success_response(token) : unauthorized_response 
-
   end
 
   def generate_token(username, password)
@@ -20,5 +22,12 @@ class LoginController < MainController
 
   def success_response(token)
     build_response(token, status: 200) 
+  end
+
+  private
+
+  def get_user_and_password(request)
+    params = JSON.parse(request.body.read)
+    [params["username"], params["password"]]
   end
 end
